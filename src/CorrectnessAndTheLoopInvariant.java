6553277c960422b621efe747331d77281ef6b50c
OriginalCoke <whuang36@hawk.iit.edu>
2019-01-19 12:40:47 -0600

    Correctness and the Loop Invariant

---
+++ src/CorrectnessAndTheLoopInvariant.java
@@ -0,0 +1,9 @@
+public class CorrectnessAndTheLoopInvariant {
+    //Correctness and the Loop Invariant
+    static int introTutorial(int V, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == V) return i;
+        }
+        return -1;
+    }
+}
